883b094d6c83eadb4622ad3781bd9ec550f32f91
abdullah <vatan7910@mynet.com>
2015-03-31 01:09:46 +0300

    etherct paketi gönderen ilk checkin

---
+++ EthercatFuzzer/EthercatFuzzer/Class1.cs
@@ -0,0 +1,50 @@
+﻿using PacketDotNet;
+using PacketDotNet.Utils;
+using SharpPcap;
+
+namespace EthercatFuzzer
+{
+    class Class1
+    {
+        static public byte [] ethernetBytes= new byte[160];
+        static ByteArraySegment array = new ByteArraySegment(ethernetBytes);
+        EthernetPacket ethernet;
+        ICaptureDevice selecteddev;
+        public Class1  ()
+        {
+            
+            // Retrieve all capture devices
+            var devices = CaptureDeviceList.Instance;
+            // devivelerden kullandığını seç
+            selecteddev = devices[1];
+            // göndermek için aç
+            selecteddev.Open();
+            // hedef mec oluştur son byteyi değiştiriyor:19
+            var desMAC= selecteddev.MacAddress;
+            var desMACBytes=desMAC.GetAddressBytes();
+            desMACBytes[5]=19;
+            //paketi oluştur
+            ethernet=new EthernetPacket(selecteddev.MacAddress,new System.Net.NetworkInformation.PhysicalAddress(desMACBytes),EthernetPacketType.EtherCatProtocol);
+            // data oluştur
+            for (int i = 0; i < 100; i++)
+            {
+                ethernetBytes[i] = 100;
+            }
+            // datayı pakete ekle
+            ethernet.PayloadData = ethernetBytes;
+            // 100 kere gönder
+            
+            
+        }
+
+
+
+        internal void gonder()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                selecteddev.SendPacket(ethernet);
+            }
+        }
+    }
+}
---
+++ EthercatFuzzer/EthercatFuzzer/Form1.cs
@@ -0,0 +1,24 @@
+﻿
+
+using PacketDotNet;
+
+namespace EthercatFuzzer
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Class1 c = new Class1();
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            c.gonder();
+        }
+    }
+}
