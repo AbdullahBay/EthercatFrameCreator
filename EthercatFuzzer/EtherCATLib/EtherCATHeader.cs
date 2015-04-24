1ac679eef648380fe7f4757f266f1c3b058307ee
nisaturkmen <nk.turkmen.ntr@gmail.com>
2015-04-24 15:04:08 +0300

    header ve datagram eklendi

---
+++ EthercatFuzzer/EtherCATLib/EtherCATHeader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtherCATLib
+{
+    class EtherCATHeader
+    {
+       
+        private Int16 eLen;
+
+         public Int16 ELength
+        {
+            get { return eLen; }
+
+            set { eLen = value; }
+        }
+         private byte eRes;
+
+         public byte EReserved
+         {
+             get { return eRes; }
+             set { eRes = value; }
+         }
+         private byte eType;
+
+         public byte EType
+         {
+             get { return eType; }
+             set { eType = value; }
+         }
+
+         public EtherCATHeader(Int16 ELength, byte EReserved, byte EType)
+         {
+             ELength = 62;
+             EType = 1;
+             EReserved = 0;
+
+             this.ELength = ELength;
+             this.EReserved = EReserved;
+             this.EType = EType;
+
+            
+           //   Int16 hd = EReserved * Math.Pow(2, 11) + ELength + EType * Math.Pow(2, 12);
+             //byte hd=0;
+             //hd = EReserved.ToString(byte)*Math.Pow(2, 11) + ELength + EType * Math.Pow(2, 12);
+
+
+
+
+         }
+      
+      
+        
+        
+    }
+}
