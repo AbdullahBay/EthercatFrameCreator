f2954391614eb5ce00859decd4da804867fdc9b6
nisaturkmen <nk.turkmen.ntr@gmail.com>
2015-04-24 15:22:36 +0300

    kütüphaneleri denemk için test projesi oluşturuldu

---
+++ EthercatFuzzer/EthercatTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthercatTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2222;
+            byte[] inte = BitConverter.GetBytes(a);
+        }
+    }
+}
