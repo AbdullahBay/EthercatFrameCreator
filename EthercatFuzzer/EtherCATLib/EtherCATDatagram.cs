ea4f7a9c188898291e0978f681e5584869748727
nisaturkmen <nk.turkmen.ntr@gmail.com>
2015-05-01 16:44:03 +0300

    datagram

--- EthercatFuzzer/EtherCATLib/EtherCATDatagram.cs
+++ EthercatFuzzer/EtherCATLib/EtherCATDatagram.cs
@@ -11,7 +11,6 @@ namespace EtherCATLib
         //TODO: paramereleri verilecek
         public DatagramHeader Header = new DatagramHeader(8, 25, 0, 46, 1, 0, 0, 0, 1) 
         { 
-            //TODO : datanın byte cinsinden uzunluğu
             Len=61
         };
         private byte[] data;
@@ -21,20 +20,6 @@ namespace EtherCATLib
             get { return data; }
             set { data = value; }
         }
-        //TODO: WKC ne olacak bak sayfa 28
-        private Int16 wkc=0;
-        /// <summary>
-        /// Working Counter: varsayılan değer 0 dır
-        /// Read Command:Success +1
-        /// Write Command:Success +1
-        /// Read Wriete Command: Success Read: +1 Success Write: +2  Success Read Wriete: +3
-        /// </summary>
-        public Int16 Wkc
-        {
-            get { return wkc; }
-            set { wkc = value; }
-        }
-
 
         /// <summary>
         /// 10 bytelik dizi döndürür.
