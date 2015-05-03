7780c1a5d01c8a88d5828e8585972e62e8cac0ec
AbdullahBay <abdullahsalih1@hotmail.com>
2015-05-03 19:37:17 +0300

    CMd listesi eklendi

--- EthercatFuzzer/EthercatFuzzer.Types/FieldList/EtherCATHeaderList.cs
+++ EthercatFuzzer/EthercatFuzzer.Types/FieldList/EtherCATHeaderList.cs
@@ -8,17 +8,13 @@ namespace EthercatFuzzer.Types.FieldList
 {
     public class EtherCATHeaderList
     {
-        /*public static List<CmdContract> CmdListe = new List<CmdContract>(){
+        public  static List<CmdContract> CmdList = new List<CmdContract>(){
                 new CmdContract(){Code=0,Abbreviation="NOP",Name="No Operation",Description="Slave ignores command"},
                 new CmdContract(){Code=1,Abbreviation="APRD",Name="Auto Increment Read",Description="Slave increments address.Slave puts read data into the EtherCAT datagram if received address is zero."}
         };
-        public static List<CmdContract> CmdList()
-        {
-            return CmdListe;
-        }*/
 
     }
-    class CmdContract{
+    public class CmdContract{
         public int Code;
         public String 
             Abbreviation,
