89828f8fe1962d0118c4e50cbbb581a7da41919b
AbdullahBay <abdullahsalih1@hotmail.com>
2015-05-05 02:44:51 +0300

    todo elimde kalmasın

--- EthercatFuzzer/EthercatFuzzer.Types/FieldList/EtherCATHeaderList.cs
+++ EthercatFuzzer/EthercatFuzzer.Types/FieldList/EtherCATHeaderList.cs
@@ -8,6 +8,7 @@ namespace EthercatFuzzer.Types.FieldList
 {
     public class EtherCATHeaderList
     {
+        //TODO: Dökumandan cmd listsinin tamammı eklenecek.
         public  static List<CmdContract> CmdList = new List<CmdContract>(){
                 new CmdContract(){Code=0,Abbreviation="NOP",Name="No Operation",Description="Slave ignores command"},
                 new CmdContract(){Code=1,Abbreviation="APRD",Name="Auto Increment Read",Description="Slave increments address.Slave puts read data into the EtherCAT datagram if received address is zero."}
